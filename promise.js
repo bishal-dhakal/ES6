fe70888c0a443df005b2c9f2caf2d7c34f465e55
bishal-dhakal <bishaldhakal95@yahoo.com>
2023-03-30 13:10:45 +0545

    to side branch

--- promise.js
+++ promise.js
@@ -2,15 +2,31 @@
 //produce promise
 const prom = new Promise( (resolve,reject) =>{
     setTimeout( () =>{
-        let rollno = [1,2,3,4,5];
-        resolve(rollno);
+        let roll_no = [1,2,3,4,5];
+        resolve(roll_no);
         reject('Error occured');
     },2000 )
 });
 
+const getInfo = (data) =>{
+    return  new Promise( ( resolve, reject) =>{
+        setTimeout( (data)=>{
+            let info = {
+                name : 'john',
+                age : 24
+            }
+            resolve(`${data} , ${info.name} and ${info.age}`)
+            reject('Couldnot get data');
+        },2000,data)
+    });
+}
+
 // promise consume
 prom.then((rollno) => {
     console.log(rollno);
-}).catch((err)=>{
-    console.log(err);
+    return getInfo(rollno[1]);
+    }).then( (dat) =>{
+        console.log(dat);
+    }).catch((err)=>{
+     console.log(err);
 })
